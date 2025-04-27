b11111ba3059834f5bb8b07280c1a586ed2a7f13
Hector Fontanez <cs.professor.fontanez@gmail.com>
2025-04-27 13:52:18 -0500

    GA4 corrections

--- my-app/client/src/analytics.js
+++ my-app/client/src/analytics.js
@@ -1,9 +1,13 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize("G-94RQYQXZ88");
+  ReactGA.initialize("G-94RQYQXZ88"); // Make sure this ID matches your GA4 property
 };
 
 export const logPageView = (path) => {
-  ReactGA.send({ hitType: "pageview", page: path });
+  ReactGA.event('page_view', {
+    page_location: window.location.href,
+    page_path: path,
+    page_title: document.title
+  });
 };

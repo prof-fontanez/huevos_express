53a4988717be451b5cefa3d905d36ee17ca19d3f
Hector Fontanez <cs.professor.fontanez@gmail.com>
2025-04-27 14:19:58 -0500

    analytics.js changes

--- my-app/client/src/analytics.js
+++ my-app/client/src/analytics.js
@@ -2,9 +2,11 @@ import ReactGA from "react-ga4";
 
 export const initGA = () => {
   ReactGA.initialize("G-94RQYQXZ88"); // Make sure this ID matches your GA4 property
+  console.log("GA Initialization Triggered");
 };
 
 export const logPageView = (path) => {
+  console.log("Page view logged: ", path); 
   ReactGA.event('page_view', {
     page_location: window.location.href,
     page_path: path,

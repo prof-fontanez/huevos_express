6e0af6fb8737bc72609357a910d98be429df489b
Hector Fontanez <cs.professor.fontanez@gmail.com>
2025-07-06 19:44:59 -0500

    Added last update timestamp to the footer

--- my-app/client/src/components/Footer.jsx
+++ my-app/client/src/components/Footer.jsx
@@ -4,6 +4,7 @@ import SocialMedia from './SocialMedia';
 import WebmasterInfo from './WebmasterInfo';
 import CommercialLicense from './CommercialLicense';
 import BusinessAddress from './BusinessAddress';
+import dayjs from 'dayjs';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
@@ -23,8 +24,11 @@ const Footer = () => {
             <SocialMedia />
             <CommercialLicense />
             <WebmasterInfo />
-            <Typography variant="body2" align="center" sx={{ p: 2, textAlign: 'center'}}>
+            <Typography variant="body2" align="center" sx={{ p: 2, textAlign: 'center' }}>
                 © 2025 - {currentYear} Huevos Express PR. Derechos Reservados.
+            </Typography>
+            <Typography variant="caption" textAlign="center" >
+                Última actualización: {dayjs().format('D [de] MMMM [de] YYYY[,] h:mm A')}
             </Typography>
 
         </Box>

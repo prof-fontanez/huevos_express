81157c8bfc59c08ee81d9c00463664ad7cff2cc3
Hector Fontanez <cs.professor.fontanez@gmail.com>
2026-04-24 18:48:56 -0500

    removed consent block from analytics.jsx

--- my-app/client/src/analytics.jsx
+++ my-app/client/src/analytics.jsx
@@ -3,14 +3,6 @@ import ReactGA from "react-ga4";
 export const initGA = () => {
   if (import.meta.env.DEV) console.log("Initializing GA with ID G-94RQYQXZ88");
 
-  // Set default consent
-  window.gtag = window.gtag || function () { window.dataLayer.push(arguments); };
-  window.dataLayer = window.dataLayer || [];
-  window.gtag('consent', 'default', {
-    'ad_storage': 'granted',
-    'analytics_storage': 'granted'
-  });
-
   // Initialize GA4 with debug_mode enabled
   ReactGA.initialize("G-94RQYQXZ88", {
     debug_mode: import.meta.env.DEV, // <-- Fix: Include debug_mode inside the options object

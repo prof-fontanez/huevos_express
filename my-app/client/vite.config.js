3404e3a7f9bbed88c9b5aac43e209217ad6c46bc
Hector Fontanez <cs.professor.fontanez@gmail.com>
2026-04-24 00:19:41 -0500

    feat: add Open Graph meta tags, sitemap, accessibility aria-labels

--- my-app/client/vite.config.js
+++ my-app/client/vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.huevosexpresspr.com',
+      routes: [
+        '/',
+        '/product',
+        '/history',
+        '/heroes',
+        '/activities',
+      ],
+    }),
+  ],
   server: {
+    host: '127.0.0.1',
     port: 3000,
     proxy: {
       '/api': 'http://localhost:5000'
